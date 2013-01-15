8443dc20ced92de3e64988e0851007d5982c7126
Fernando Serapio <fhernandosc@gmail.com>
2013-01-15 02:14:19 +0100

    created user relationships infraestructure

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   before_validation :prep_email
   before_save :create_avatar_url
   has_many :ribbits
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
 
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
@@ -11,6 +15,14 @@ class User < ActiveRecord::Base
 
   def create_avatar_url
   	self.avatar_url = "http://gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+  end
+
+  def following? user
+    self.followeds.include? user
+  end
+
+  def follow user
+    Relationship.create follower_id: self.id, followed_id: @user.id
   end
 
   private
