feb2efdcecfcad69b281dee2f860ffc039565352
Fernando Serapio <fhernandosc@gmail.com>
2013-01-15 04:17:53 +0100

    added buddies page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 	def new
-		@user = User.new
+		if current_user
+			redirect_to buddies_path
+		else
+			@user = User.new
+		end
 	end
 
 	def create
@@ -17,5 +21,19 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@ribbit = Ribbit.new
 		@relationship = Relationship.where(follower_id: current_user.id, followed_id: @user.id).first_or_initialize if current_user
+	end
+
+	def index
+		@users = User.all
+	end
+
+	def buddies
+		if current_user
+			@ribbit = Ribbit.new
+			buddies_ids = current_user.followers.map(&:id).push current_user.id
+			@ribbits = Ribbit.find_all_by_user_id buddies_ids
+		else
+			redirect_to root_url
+		end
 	end
 end
